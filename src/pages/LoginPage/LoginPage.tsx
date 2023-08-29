67860bc781dd425394e7ddc4e9ef4224088f2b6a
Joan Pérez <joanperez.lapuente@gmail.com>
2023-08-30 01:01:18 +0200

    Add styles LoginPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -2,6 +2,7 @@ import { signInWithPopup, signOut } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { auth, githubAuthProvider } from "../../firebase";
+import "./LoginPage.css";
 
 const LoginPage = (): React.ReactElement => {
   const [user] = useAuthState(auth);
